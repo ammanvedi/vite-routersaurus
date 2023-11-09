1dc9321041b9cb61d5a05b5bd7d4d5f102f0355c
Amman Vedi <av@Ammans-MacBook-Pro.local>
2023-11-09 21:40:27 +0000

    v0.1.0

---
+++ apps/e2e/cypress/e2e/layout.cy.ts
@@ -0,0 +1,16 @@
+describe('Routersaurus Vite E2E', () => {
+
+  before(() => {
+    cy.visit('http://localhost:8008')
+  })
+
+  describe('Layout', () => {
+    it('s')
+  })
+
+  it('passes', () => {
+
+
+    cy.contains('Root Layout')
+  })
+})
--- apps/e2e/cypress/e2e/spec.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-
-    cy.visit('http://localhost:8008')
-    cy.contains('Root Layout')
-  })
-})
